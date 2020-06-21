420490ddcc9d0061d8a9a9ef2893cb0c716ebd88
Balvinder <balvinder@in-balvindk.local>
2020-06-21 17:37:44 +0530

    [Balvinder] Adding missing property

--- src/main/scala/utilities/Constants.scala
+++ src/main/scala/utilities/Constants.scala
@@ -15,11 +15,12 @@ object Constants {
 
 
   val DELTA_TABLENAME = "delta_lake_inventory"
+  val DELTA_HISTORY_VERSIONING_BASEPATH = "file:///${CURRENT_DIRECTORY}/src/main/resources/history_versioning_"+DELTA_TABLENAME
   val DELTA_BASEPATH = s"file:///${CURRENT_DIRECTORY}/data/"+DELTA_TABLENAME
   val DELTA_CHECKPOINT_PATH = "file:///${CURRENT_DIRECTORY}/data/checkpoint_"+DELTA_TABLENAME
   val DELTA_STREAMING_INPUT_PATH = "file:///${CURRENT_DIRECTORY}/data/streaming_input_"+DELTA_TABLENAME
   val DELTA_STREAMING_OUTPUT_PATH = "file:///${CURRENT_DIRECTORY}/data/streaming_output_"+DELTA_TABLENAME
-  
+
   val SMALL_PARQUET_FILE_SPATH="file:///Users/in-svsarang/Desktop/sarang/sparkwork/src/main/resources/parquet_small_files"
   val COMPACTED_MOR_HUDI_FILES_PATH="file:///Users/in-svsarang/Desktop/sarang/sparkwork/src/main/resources/compacted_hudi_mor_files"
   val COMPACTED_MOR_INLINE_COMPACT_HUDI_FILES_PATH="file:///Users/in-svsarang/Desktop/sarang/sparkwork/src/main/resources/compacted_hudi_mor_inline_compact_files"
